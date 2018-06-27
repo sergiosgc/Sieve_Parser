a9021acfb896dc70fc687e14cd42592d9ee412a9
Sérgio Carvalho <sergio.carvalho@sooma.com>
2018-06-27 16:13:52 +0100

    Small fix on Reader

--- lib/Reader.php
+++ lib/Reader.php
@@ -7,12 +7,9 @@ class Reader {
             $str,
             new \sergiosgc\Text_Tokenizer_Regex_Matcher_Php());
 
-        $parser = new TestSieveParser(
-            new \sergiosgc\Sieve_Parser\Tokenizer(
-                $str,
-                new \sergiosgc\Text_Tokenizer_Regex_Matcher_Php()
-            ));
-        return $parser->parse()->getValue();
+        $parser = new Parser($tokenizer);
+        return new Script($parser->parse()->getValue());
+
     }
 }
 
